8c4b46261e1a27b5b0ccad140ded9f04d5228664
Julia <julial.herand@educa.jcyl.es>
2024-02-08 17:56:57 +0100

    sql corregido

--- registro.sql
+++ registro.sql
@@ -3,12 +3,12 @@ create database Registro;
 
 use registro;
 
-create table usuario{
+create table usuario(
     id int auto_increment,
     nombre varchar(50) unique,
     contraseña varchar(50),
     primary key(id)
-}
+);
 
 insert into usuario(nombre, contraseña) values ("Pepe", "jope33");
 insert into usuario(nombre, contraseña) values ("Marta", "laTarta");
