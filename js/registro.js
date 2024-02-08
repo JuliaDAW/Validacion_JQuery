eff0900c69a4b540bb7abfc24240f8112a411b97
Fernando <ferestga8@gmail.com>
2024-02-08 17:11:34 +0100

    Añadido la validación en registro

--- js/registro.js
+++ js/registro.js
@@ -1,1 +1,20 @@
-$("#id_registrase").validate()
+$("#id_registrarse").validate({
+    rules: {
+        nombre: {
+            required: true,
+            minlength: 3
+        },
+        correo: {
+            required: true,
+            email: true
+        },
+        contrasenia: {
+            required: true,
+            minlength: 6
+        },
+        repetir: {
+            required: true,
+            equalTo: "#id_contrasenia"
+        }
+    }
+})
