4f8e08eac6a466df4b14ac0235974eb086107f08
Julia <julial.herand@educa.jcyl.es>
2024-02-08 17:53:48 +0100

    sql

---
+++ registro.sql
@@ -0,0 +1,16 @@
+drop database if exists Registro;
+create database Registro;
+
+use registro;
+
+create table usuario{
+    id int auto_increment,
+    nombre varchar(50) unique,
+    contraseña varchar(50),
+    primary key(id)
+}
+
+insert into usuario(nombre, contraseña) values ("Pepe", "jope33");
+insert into usuario(nombre, contraseña) values ("Marta", "laTarta");
+insert into usuario(nombre, contraseña) values ("Helena", "hell666");
+insert into usuario(nombre, contraseña) values ("Manolo", "manolin");
