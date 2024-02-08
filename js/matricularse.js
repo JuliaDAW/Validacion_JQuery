caf806e6baf50e8b649e7d8e106f8e487ad6fd77
Fernando <ferestga8@gmail.com>
2024-02-08 17:31:27 +0100

    validacion

--- js/matricularse.js
+++ js/matricularse.js
@@ -20,6 +20,30 @@ $("#id_matricularse").validate({
             required: true,
             date: true
         },
+        pais: {
+            required: true
+        },
+        provincia: {
+            required: true
+        },
+        telefono: {
+            required: true,
+            minlength: 9
+        },
+        movil: {
+            required: true,
+            minlength: 9
+        },
+        correo: {
+            required: true,
+            email: true
+        },
+        estudio: {
+            required: true
+        },
+        repetir: {
+            required: true,
+        }
     },
     messages: {
         nombre: {
@@ -41,6 +65,27 @@ $("#id_matricularse").validate({
         nacer: {
             required: "Por favor, ingrese su fecha de nacimiento",
             date: "Por favor, ingrese una fecha válida"
-        }
+        },
+        pais: {
+            required: "Por favor, seleccione su país"
+        },
+        provincia: {
+            required: "Por favor, seleccione su provincia"
+        },
+        telefono: {
+            required: "Por favor, ingrese su teléfono",
+            minlength: "El teléfono debe tener al menos 9 caracteres"
+        },
+        correo: {
+            required: "Por favor, ingrese su correo",
+            email: "Por favor, ingrese un correo válido"
+        },
+        estudio: {
+            required: "Por favor, seleccione su nivel de estudios"
+        },
+        repetir: {
+            required: "Por favor, seleccione si desea repetir el curso"
+        },
+
     }
 })
